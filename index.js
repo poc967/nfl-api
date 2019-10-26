e968445cb47aa307ccba0fb931ed2442d5f8f281
Patrick O'Connor <poc.967@gmail.com>
2019-10-26 15:23:00 -0400

    added sequelize

--- index.js
+++ index.js
@@ -5,37 +5,37 @@ const bodyParser = require('body-parser')
 const app = express()
 
 app.get('/teams/:id', (request, response) => {
-    const id = request.params.id
+  const id = request.params.id
 
-    const matchingTeam = teams.filter((team) => {
-        return team.id === Number(id) || team.abbreviation === id.toUpperCase()
-    })
+  const matchingTeam = teams.filter((team) => {
+    return team.id === Number(id) || team.abbreviation === id.toUpperCase()
+  })
 
-    if (matchingTeam.length) {
-        response.send(matchingTeam)
-    } else {
-        response.sendStatus(418)
-    }
+  if (matchingTeam.length) {
+    response.send(matchingTeam)
+  } else {
+    response.sendStatus(418)
+  }
 })
 
 app.get('/teams', (request, response) => {
-    response.send(teams)
+  response.send(teams)
 })
 
 app.post('/teams', bodyParser.json(), (request, response) => {
-    const { id, location, mascot, abbreviation, conference, division } = request.body
+  const { id, location, mascot, abbreviation, conference, division } = request.body
 
-    if (!id || !location || !mascot || !abbreviation || !conference || !division) {
-        response.status(400).send('The following attributes are required: id, location, mascot, abbreviation, conference, division')
-    } else {
-        const newTeam = { id, location, mascot, abbreviation, conference, division }
-        teams.push(newTeam)
-        response.status(201).send(newTeam)
-    }
+  if (!id || !location || !mascot || !abbreviation || !conference || !division) {
+    response.status(400).send('The following attributes are required: id, location, mascot, abbreviation, conference, division')
+  } else {
+    const newTeam = { id, location, mascot, abbreviation, conference, division }
+    teams.push(newTeam)
+    response.status(201).send(newTeam)
+  }
 })
 
 app.all('*', (request, response) => {
-    response.sendStatus(404)
+  response.sendStatus(404)
 })
 
 const server = app.listen(8080, () => { console.log('Listening on port 8080...') })
